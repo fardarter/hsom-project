046bb81c40844a5805da646a8af2df9e46b7110b
saul <saul@nml.co.za>
2018-12-23 15:01:52 +0200

    feat(Starter): Add starter with Euterpea and HSoM

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for hsklsom
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import HSoM.Examples.MUIExamples2
+import Euterpea
+import Lib
+
+main :: IO ()
+main = someFunc
+
+testNote :: IO ()
+testNote = play $ c 4 qn
+
+testMui = bifurcate
